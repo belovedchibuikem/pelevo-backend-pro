e2216fdce70d9ece2116ef5a1db80e9786da7dae
belovedchibuikem <johnchibuikem20@gmail.com>
2025-07-20 16:57:23 +0100

    Restrict Data

--- app/Services/PodcastIndexService.php
+++ app/Services/PodcastIndexService.php
@@ -33,7 +33,7 @@ class PodcastIndexService
             'X-Auth-Date' => $apiHeaderTime,
             'X-Auth-Key' => $this->apiKey,
             'Authorization' => $sha1Hash,
-            'User-Agent' => config('app.name', 'Pelevo') . '/1.3'
+            'User-Agent' => config('app.name', 'Pelevo')
         ];
     }
 
